2bc1662e4835b85bed5605df92e54068080a895d
Sidronio Lima <sidronio@sidroniolima.com.br>
2017-02-15 11:28:26 -0200

    Modal de site em construcao

--- js/datepicker.js
+++ js/datepicker.js
@@ -1660,9 +1660,9 @@
 		en: {
 			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
 			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			daysMin: ["S", "T", "Q", "Q", "S", "S", "D"],
+			months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+			monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Otu", "Nov", "Dez"],
 			today: "Today",
 			clear: "Clear",
 			titleFormat: "MM yyyy"
