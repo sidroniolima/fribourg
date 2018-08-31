0bdd748c8b3e6bb40e750060ca0072dcd80f1ee4
Sidronio Lima <sidronio@sidroniolima.com.br>
2018-08-31 15:36:29 -0300

    Criacao da listagem de comuunicados

---
+++ js/comunicados.json.js
@@ -0,0 +1,15 @@
+const comunicados = [
+  { "id": 1, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 2, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 3, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 4, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 5, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 6, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 7, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 8, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 9, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 10, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 11, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 12, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
+  { "id": 13, "data": "07/09/2018", "titulo": "Teste de comunicado (Teste)", "categoria": "fundamental+2"}
+];
