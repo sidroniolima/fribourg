ba5a66443d88b231fd05f247a9a39afa51093639
Sidronio Lima <sidronio@sidroniolima.com.br>
2018-09-05 16:35:39 -0300

    Criacao das paginas dos comunicados e links

--- js/comunicados.json.js
+++ js/comunicados.json.js
@@ -1,15 +1,5 @@
 const comunicados = [
-  { "id": 1, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 2, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 3, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 4, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 5, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 6, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 7, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 8, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 9, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 10, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 11, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 12, "data": "01/09/2018", "titulo": "Provas Bimestrais (Teste)", "categoria": "fundamental+1"},
-  { "id": 13, "data": "07/09/2018", "titulo": "Teste de comunicado (Teste)", "categoria": "fundamental+2"}
+  { "id": 1, "url": "provas_bimestrais_3_bim", "data": "05/09/2018", "titulo": "Calendário de Provas Bimestrais 3º Bim.", "categoria": "medio"},
+  { "id": 2, "url": "provas_bimestrais_3_bim", "data": "05/09/2018", "titulo": "Calendário de Provas Bimestrais 3º Bim.", "categoria": "fundamental+1"},
+  { "id": 3, "url": "provas_bimestrais_3_bim", "data": "05/09/2018", "titulo": "Calendário de Provas Bimestrais 3º Bim.", "categoria": "fundamental+2"},
 ];
