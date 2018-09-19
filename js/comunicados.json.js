35e322a22f19f837cdeebff4adb54e502dcb8cad
Sidronio Lima <sidronio@sidroniolima.com.br>
2018-09-19 14:15:48 -0300

    Circular 15

--- js/comunicados.json.js
+++ js/comunicados.json.js
@@ -2,4 +2,5 @@ const comunicados = [
   { "id": 1, "url": "provas_bimestrais_3_bim", "data": "05/09/2018", "titulo": "Calendário de Provas Bimestrais 3º Bim.", "categoria": "medio"},
   { "id": 2, "url": "provas_bimestrais_3_bim", "data": "05/09/2018", "titulo": "Calendário de Provas Bimestrais 3º Bim.", "categoria": "fundamental+1"},
   { "id": 3, "url": "provas_bimestrais_3_bim", "data": "05/09/2018", "titulo": "Calendário de Provas Bimestrais 3º Bim.", "categoria": "fundamental+2"},
+  { "id": 4, "url": "circular_15", "data": "17/09/2018", "titulo": "Conteúdo das Provas Bimestrais 3º Bim.", "categoria": "fundamental+1"},
 ];
