b0c49014342c6a4607dc11058edafe571999b815
Sidronio Lima <sidronio@sidroniolima.com.br>
2018-11-10 13:11:17 -0200

    Comunicados 46 - Fundamental

--- js/comunicados.json.js
+++ js/comunicados.json.js
@@ -1,11 +1,9 @@
 const comunicados = [
-  { "id": 9, "url": "circular_38_1_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 1º ano", "categoria": "fundamental+1"},
-  { "id": 10, "url": "circular_38_2_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 2º ano", "categoria": "fundamental+1"},
-  { "id": 11, "url": "circular_38_3_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 3º ano", "categoria": "fundamental+1"},
-  { "id": 12, "url": "circular_38_4_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 4º ano", "categoria": "fundamental+1"},
-  { "id": 13, "url": "circular_38_5_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 5º ano", "categoria": "fundamental+1"},
-  { "id": 14, "url": "circular_40_6_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 6º ano", "categoria": "fundamental+2"},
-  { "id": 15, "url": "circular_40_7_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 7º ano", "categoria": "fundamental+2"},
-  { "id": 16, "url": "circular_40_8_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 8º ano", "categoria": "fundamental+2"},
-  { "id": 17, "url": "circular_40_9_ano", "data": "21/10/2018", "titulo": "Conteúdo das Mensais do 4º Bim. 9º ano", "categoria": "fundamental+2"}
+  { "id": 18, "url": "circular_46_6_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 6º ano", "categoria": "fundamental+2"},
+  { "id": 19, "url": "circular_46_7_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 7º ano", "categoria": "fundamental+2"},
+  { "id": 20, "url": "circular_46_8_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 8º ano", "categoria": "fundamental+2"},
+  { "id": 21, "url": "circular_46_9_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 9º ano", "categoria": "fundamental+2"},
+  { "id": 22, "url": "circular_46_1_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 1º ano", "categoria": "medio"},
+  { "id": 23, "url": "circular_46_2_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 2º ano", "categoria": "medio"},
+  { "id": 24, "url": "circular_46_3_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 3º ano", "categoria": "medio"},
 ];
