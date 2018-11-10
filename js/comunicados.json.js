4f0c20e665915881e301fb537195735664f79210
Sidronio Lima <sidronio@sidroniolima.com.br>
2018-11-10 13:38:39 -0200

    Comunicados 46 - Medio

--- js/comunicados.json.js
+++ js/comunicados.json.js
@@ -2,8 +2,8 @@ const comunicados = [
   { "id": 18, "url": "circular_46_6_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 6º ano", "categoria": "fundamental+2"},
   { "id": 19, "url": "circular_46_7_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 7º ano", "categoria": "fundamental+2"},
   { "id": 20, "url": "circular_46_8_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 8º ano", "categoria": "fundamental+2"},
-  { "id": 21, "url": "circular_46_9_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 9º ano", "categoria": "fundamental+2"},
-  { "id": 22, "url": "circular_46_1_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 1º ano", "categoria": "medio"},
-  { "id": 23, "url": "circular_46_2_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 2º ano", "categoria": "medio"},
-  { "id": 24, "url": "circular_46_3_ano", "data": "21/11/2018", "titulo": "Conteúdo das Semestrais 3º ano", "categoria": "medio"},
+  { "id": 21, "url": "circular_46_9_ano", "data": "13/11/2018", "titulo": "Conteúdo das Semestrais 9º ano", "categoria": "fundamental+2"},
+  { "id": 22, "url": "circular_46_1_ano", "data": "14/11/2018", "titulo": "Conteúdo das Semestrais 1º ano", "categoria": "medio"},
+  { "id": 23, "url": "circular_46_2_ano", "data": "14/11/2018", "titulo": "Conteúdo das Semestrais 2º ano", "categoria": "medio"},
+  { "id": 24, "url": "circular_46_3_ano", "data": "13/11/2018", "titulo": "Conteúdo das Semestrais 3º ano", "categoria": "medio"},
 ];
