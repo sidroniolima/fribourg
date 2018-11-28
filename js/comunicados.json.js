229d9fcd89f3f7ea0739c88b981a5a738ff55e74
Sidronio Lima <sidronio@sidroniolima.com.br>
2018-11-28 08:47:24 -0200

    Circular 46 fund. 1

--- js/comunicados.json.js
+++ js/comunicados.json.js
@@ -6,4 +6,9 @@ const comunicados = [
   { "id": 22, "url": "circular_46_1_ano", "data": "14/11/2018", "titulo": "Conteúdo das Semestrais 1º ano", "categoria": "medio"},
   { "id": 23, "url": "circular_46_2_ano", "data": "14/11/2018", "titulo": "Conteúdo das Semestrais 2º ano", "categoria": "medio"},
   { "id": 24, "url": "circular_46_3_ano", "data": "13/11/2018", "titulo": "Conteúdo das Semestrais 3º ano", "categoria": "medio"},
+  { "id": 25, "url": "circular_46_1_ano", "data": "23/11/2018", "titulo": "Conteúdo das Semestrais 1º ano", "categoria": "fundamental+1"},
+  { "id": 26, "url": "circular_46_2_ano", "data": "23/11/2018", "titulo": "Conteúdo das Semestrais 2º ano", "categoria": "fundamental+1"},
+  { "id": 27, "url": "circular_46_3_ano", "data": "23/11/2018", "titulo": "Conteúdo das Semestrais 3º ano", "categoria": "fundamental+1"},
+  { "id": 28, "url": "circular_46_4_ano", "data": "23/11/2018", "titulo": "Conteúdo das Semestrais 4º ano", "categoria": "fundamental+1"},
+  { "id": 28, "url": "circular_46_5_ano", "data": "22/11/2018", "titulo": "Conteúdo das Semestrais 5º ano", "categoria": "fundamental+1"},
 ];
